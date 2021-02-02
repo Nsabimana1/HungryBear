c513de343abe250499c9dcadcbaf4c5fcc667ea4
Josh <thomeczekje@hendrix.edu>
2021-02-02 13:28:51 -0600

    Added art stuff, physics scripts, and mouse movement

---
+++ HungryBear/Assets/Scripts/PancakeBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PancakeBehavior : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(transform.right * 120);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
